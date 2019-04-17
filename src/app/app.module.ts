067393fe55aaf8f6afb638397688a2e6d3b7848e
Toby Mills <toby_mills@trimble.com>
2019-04-17 09:29:52 +0200

    Fixed penoc-sdk import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { PenocSdkModule } from '../../node_modules/penoc-sdk/penoc-sdk.module';
+import { PenocSdkModule } from 'penoc-sdk/penoc-sdk.module';
 import { AppComponent } from './components/app/app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { ButtonBarComponent } from './components/button-bar/button-bar.component';
